3fcc4add112b5469f0d632cd57e1293a6554d41f
alex-clements <ahclemen@ualberta.ca>
2020-11-15 21:36:17 -0800

    existance is pain update

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          this is my domain now.
+          existance is pain.
         </p>
 
         <div className={styles.grid}>
